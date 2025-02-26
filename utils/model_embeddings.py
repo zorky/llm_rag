94c4d2a04e05890cc7396981666e83b724ecced0
DUVAL Olivier <zorky00@gmail.com>
2025-02-26 15:40:33 +0100

    Update model_embeddings.py : +lien vers CamemBERT HF

--- utils/model_embeddings.py
+++ utils/model_embeddings.py
@@ -1,6 +1,9 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
 def get_model_name():
+    """
+    Modèle CamemBERT spécialisé en français : https://huggingface.co/dangvantuan/sentence-camembert-large
+    """
     return "dangvantuan/sentence-camembert-large"
 
 def get_embedding_model():
