19ec423d41dc37c6d086535fb61b9cf78c1d9404
Olivier Duval <olivier.duval@univ-paris8.fr>
2025-03-03 18:22:41 +0100

    +possibilité de charger tous les documents du répertoire kb ou uniquement le PDF exemple

--- index-doc.py
+++ index-doc.py
@@ -1,5 +1,6 @@
 import chromadb
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
+from langchain_community.document_loaders.pdf import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from utils.constants import CHROMA_DB, CHROMA_COLLECTION, CHUNK_SIZE, CHUNK_OVERLAP
@@ -7,11 +8,25 @@ from utils.logger import init_logger
 from utils.model_embeddings import get_embedding_model_chroma
 from utils.measure_time_decorator import measure_time
 
-KB_DOC = "kb/harcelement-ecole.pdf"
+KB_DIR = "kb"
+KB_DOC_EXAMPLE = f"{KB_DIR}/harcelement-ecole.pdf"
 IDX_PREFIX = "doc_"
+LOAD_ALL_DOCS = True
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader(KB_DIR)
+    return document_loader.load()
+
+def _get_documents_or_pages(load_all_docs) -> list[Document]:
+    if load_all_docs:
+        documents = load_documents()
+    else:
+        pdf_loader = PyPDFLoader(KB_DOC_EXAMPLE)
+        documents = pdf_loader.load()
+    return documents
 
 @measure_time
-def create_db():
+def create_db(load_all_docs=True or LOAD_ALL_DOCS):
     """
     Init ChromaDB et indexe le texte d'un document PDF avec embeddings (vecteurs)
     """
@@ -20,25 +35,27 @@ def create_db():
     chroma_client = chromadb.PersistentClient(path=CHROMA_DB)
     collection = chroma_client.get_or_create_collection(name=CHROMA_COLLECTION)
 
-    # Charger et découper un document PDF
-    pdf_loader = PyPDFLoader(KB_DOC)
-    pages = pdf_loader.load()
+    documents = _get_documents_or_pages(load_all_docs)
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
-    texts = text_splitter.split_documents(pages)
+    texts = text_splitter.split_documents(documents)
 
     # Indexation des passages dans ChromaDB
     for i, chunk in enumerate(texts):
-        embedding_vector = embedding_model.embed_query(chunk.page_content)
-        collection.add(
-            ids=[f"{IDX_PREFIX}{i}"],
-            documents=[chunk.page_content],
-            embeddings=[embedding_vector],
-            metadatas=[{"source": KB_DOC}]
-        )
+        if i < 50: # limit car sur ma station ça plante segmentation fault
+            embedding_vector = embedding_model.embed_query(chunk.page_content)
+            print(f"{embedding_vector}")
+            collection.add(
+                ids=[f"{IDX_PREFIX}{i}"],
+                documents=[chunk.page_content],
+                embeddings=[embedding_vector],
+                metadatas=[{"source": KB_DOC_EXAMPLE}]
+            )
+        else:
+            break
 
     print(f"Indexation terminée : {len(texts)} chunks ajoutés.")
 
 if __name__ == '__main__':
     init_logger()
-    create_db()
+    create_db(load_all_docs=False)
